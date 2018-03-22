cc26f2890db783074643e2e96de51dfe26b6bcf2
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-03-22 10:48:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :cities
+  resources :neighborhoods
+  resources :listings
+  resources :reservations
+  resources :reviews
+  resources :users
   resources :users
 
   resources :listings
